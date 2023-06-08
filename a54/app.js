ca3dbbb3ce1747fa016e19aeafe86d7d27011ee6
Arthur Teixeira Serafim <arthurteixeiraserafim@gmail.com>
2023-06-07 23:25:10 -0300

    Udemy JS

--- a54/app.js
+++ a54/app.js
@@ -34,5 +34,11 @@ function f3(callback){
 f1(f1Callback);
 
 function f1Callback(){
-    
+   f2(f2Callback) 
+}
+function f2Callback(){
+    f3(f3Callback)
+}
+function f3Callback(){
+    console.log('Olá Mundo')
 }
