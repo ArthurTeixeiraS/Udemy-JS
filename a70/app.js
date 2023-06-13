67e6eb1aca69178f6e62ec09027e8d5daeb62f30
Arthur Teixeira Serafim <arthurteixeiraserafim@gmail.com>
2023-06-12 22:20:24 -0300

    Udemy Js

---
+++ a70/app.js
@@ -0,0 +1,12 @@
+//Revisão de Objetos
+const pessoa = {
+    nome: 'Arthur',
+    sobrenome: 'Teixeira',
+    idade: 17
+};
+const dado = 'idade'
+console.log(pessoa['nome']) //em alguns casos pode usar um valor dinâminco
+console.log(pessoa.sobrenome)
+console.log(pessoa[dado])
+
+ 
