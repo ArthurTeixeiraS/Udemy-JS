2acc8716048587dae9ffaba0d289a46e5e3e4541
Arthur Teixeira Serafim <arthurteixeiraserafim@gmail.com>
2023-07-20 23:05:15 -0300

    Udemy JS

---
+++ a97/src/index.js
@@ -0,0 +1,5 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import executa from './modules/promises'
+import '../public/assets/css/style.css'
+executa();
---
+++ a97/src/modules/promises.js
@@ -0,0 +1,14 @@
+
+function promise(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            console.log('Sua Promise')
+            resolve();
+        }, 2000)
+    })
+}
+
+export default async function () {
+    await promise()
+    console.log('Terminou')
+}
